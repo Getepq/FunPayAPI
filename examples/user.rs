d294f7f77f19db95cb52c8b6a767bef770595c4e
hayward <foxitime104@gmail.com>
2026-08-17 21:22:16 +0500

    docs(examples): показать как получить данные о пользователях.

---
+++ examples/user.rs
@@ -0,0 +1,19 @@
+use funpay_api::{Client, Result};
+
+
+#[tokio::main] 
+async fn main() -> Result<()> {
+    // Инициализируем в проект аккаунт для работы с ним. 
+    let client = Client::new("", None).await?;
+    
+    // Получаем данные о пользователю, кому принадлежит golden_key.
+    let user = client.get_current_user().await?;
+
+    // Выводим в консоль. Трейт Display не реализован для всех моделей.
+    println!("{:?}", user);
+
+    // Получаем данные о другом пользователя, в нашем примере это SidoRenko.
+    let sidor_user = client.get_user(714925).await?;
+    println!("{:?}", sidor_user);
+    Ok(())
+}
