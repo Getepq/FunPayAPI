2bf30db28ccae190a03cc3674cb902122378975e
hayward <foxitime104@gmail.com>
2026-08-18 17:21:42 +0500

    feat(parser): экспортировать разбор балансов

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,9 +1,11 @@
 mod chat;
 mod error;
+mod finance;
 mod offer;
 mod order;
 pub(crate) mod selectors;
 mod user;
 
 pub(crate) use error::{Error, Result};
+pub(crate) use finance::get_balances;
 pub(crate) use user::get_user;
